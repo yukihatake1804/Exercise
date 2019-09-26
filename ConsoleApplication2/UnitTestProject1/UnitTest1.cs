a2d53d8f16479cbdd59f7e5d6fd95d3c1a30b0d3
HieuWibuButNotWibu <nhokhieuthao@gmail.com>
2019-09-26 17:58:24 +0700

    OddEven

---
+++ ConsoleApplication2/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ConsoleApplication1;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestIsOdd()
+        {
+            bool result3 = Program.IsOdd(3);
+            Assert.AreEqual(true, result3);
+
+            bool result4 = Program.IsOdd(4);
+            Assert.AreEqual(false,result4);
+
+        }
+
+   
+    }
+}
---
+++ ConsoleApplication2/UnitTestProject2/UnitTest1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ConsoleApplication1;
+namespace UnitTestProject2
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestIsEven()
+        {
+            bool result3 = Program.IsEven(3);
+            Assert.AreEqual(false, result3);
+
+            bool result4 = Program.IsEven(4);
+            Assert.AreEqual(true, result4);
+        }
+    }
+}
