1582a717db092c8381e0197a7061319568ef8a68
hieu <nhokhieuthao@gmail.com>
2019-10-01 17:32:22 +0700

    CeilFloor

---
+++ ConsoleApplication3/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ConsoleApplication3;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void CeilFloor()
+        {
+            int result1 = Program.Ceil((float)2.3);
+            Assert.AreEqual(3, result1);
+
+            int result2 = Program.Ceil((float)-2.3);
+            Assert.AreEqual(-2, result2);
+
+            int result5 = Program.Ceil((float)3);
+            Assert.AreEqual(3, result5);
+
+            int result6 = Program.Floor((float)3);
+            Assert.AreEqual(3, result6);
+
+            int result3 = Program.Floor((float)2.3);
+            Assert.AreEqual(2, result3);
+
+            int result4 = Program.Floor((float)-2.3);
+            Assert.AreEqual(-3, result4);
+
+        }
+
+    }
+}
